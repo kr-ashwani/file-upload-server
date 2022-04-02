3e81fe937b4fcd63cc6b171cf8c1294956d85ac8
Ashwani Kumar <ashwanikr160@gmail.com>
2022-04-02 07:42:48 +0530

    xhr request improved

--- server/app.js
+++ server/app.js
@@ -10,9 +10,9 @@ const storage = multer.diskStorage({
     cb(null, path.join(__dirname, "/..", "uploads"));
   },
   filename: function (req, file, cb) {
-    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1e4);
     // cb(null, file.fieldname + "-" + uniqueSuffix);
-    cb(null, uniquePrefix + "-" + file.originalname);
+    cb(null, uniquePrefix + file.originalname);
   },
 });
 const upload = multer({ storage: storage });
@@ -31,26 +31,57 @@ app.set("view engine", "ejs"); //express will by default look up to views folder
 
 // Express 4.16+ has implemented their own version of body-parser so you do not need to add the dependency to your project. You can run it natively in express
 
-// app.use(express.json()); // Used to parse JSON bodies
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Used to parse JSON bodies
+app.use(express.urlencoded({ extended: true }));
 
 // for parsing multipart/form-data
 
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.post("/upload", upload.single("userFile"), (req, res) => {
-  res.render("sucessfull", {
+app.post("/upload/:uniqueRequest", upload.array("userFiles"), (req, res) => {
+  const fileArr = [];
+  console.log(req.files);
+  req.files.forEach((elem) =>
+    fileArr.push({
+      fileName: elem.originalname,
+      filePath: elem.filename,
+      fileType: elem.mimetype,
+    })
+  );
+  let oldFile = fs.readFileSync(
+    path.join(__dirname, "/..", "uploads", "uploadMap.json"),
+    "utf-8"
+  );
+  oldFile = JSON.parse(oldFile);
+  oldFile.push({
     userName: req.body.name,
-    fileName: req.file.filename,
+    roll: req.body.roll,
+    fileArr,
+    uniqueRequest: req.params.uniqueRequest,
   });
+  console.log(oldFile);
+  fs.writeFileSync(
+    path.join(__dirname, "/..", "uploads", "uploadMap.json"),
+    JSON.stringify(oldFile)
+  );
+  res.send(fileArr);
+});
+
+app.get("/previewfiles/:uniqueRequest", (req, res) => {
+  const uniqueReq = req.params.uniqueRequest;
+  let fileData = fs.readFileSync(
+    path.join(__dirname, "/..", "uploads", "uploadMap.json"),
+    "utf-8"
+  );
+  fileData = JSON.parse(fileData).filter(
+    (elem) => elem.uniqueRequest === uniqueReq
+  );
+  res.render("sucessfull.ejs", { previewData: fileData[0] });
 });
 
 app.get("/getFile/:fileName", (req, res) => {
-  fs.createReadStream(
-    path.join(__dirname, "/..", "uploads", req.params.fileName)
-  ).pipe(res);
-  // res.sendFile(path.join(__dirname, "/..", "uploads", req.params.fileName));
+  res.sendFile(path.join(__dirname, "/..", "uploads", req.params.fileName));
 });
 
 app.listen(port, () => console.log(`server is running at ${port}`));
